28629f4e5b1e28e8edf387d5c41c8ad80125631e
piercenovo <piercenovoah@gmail.com>
2023-03-22 20:07:13 -0500

    chore(Root): Setup project

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+export default function App ({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>iMessage</title>
+        <meta name='description' content='iMessage, una plataforma de mensajería instantánea diseñada para ofrecer una experiencia de comunicación rápida, segura y confiable 🚀' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicons/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
